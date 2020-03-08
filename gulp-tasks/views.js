8064ea40aff9f2e86cfa1562052e676e150bd740
vamedyakov <vladimir.mediakov@gmail.com>
2020-03-08 13:07:30 +0300

    верстка

--- gulp-tasks/views.js
+++ gulp-tasks/views.js
@@ -2,7 +2,6 @@
 
 import { paths } from "../gulpfile.babel";
 import gulp from "gulp";
-import include from "gulp-file-include";
 import gulpif from "gulp-if";
 import replace from "gulp-replace";
 import browsersync from "browser-sync";
@@ -13,10 +12,6 @@ const argv = yargs.argv,
 
 gulp.task("views", () => {
     return gulp.src(paths.views.src)
-        .pipe(include({
-            prefix: "@@",
-            basepath: "@file"
-        }))
         .pipe(gulpif(production, replace(".css", ".min.css")))
         .pipe(gulpif(production, replace(".js", ".min.js")))
         .pipe(gulp.dest(paths.views.dist))
